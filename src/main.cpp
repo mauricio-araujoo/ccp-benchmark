5fe9fb759d220adea7ea407108ab66cc66f9f9ae
Lui Habl <txaicalvoso@gmail.com>
2024-11-21 11:58:38 -0300

    Arg parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,60 @@
 #include <cstdio>
+#include <string>
+
+#include <argparse/argparse.hpp>
 
 #include "simulation.h"
 #include "kn/random/random.h"
 
+ccp::Parameters get_case_parameters(int case_number) {
 
-int main() {
+    ccp::Parameters p;
+
+    switch (case_number) {
+        case 1:
+            p = ccp::Parameters::case_1();
+        break;
+        case 2:
+            p = ccp::Parameters::case_2();
+        break;
+        case 3:
+            p = ccp::Parameters::case_3();
+        break;
+        case 4:
+            p = ccp::Parameters::case_4();
+        break;
+        default:
+        break;
+    }
+
+    return p;
+}
+
+int main(int argc, char *argv[]) {
     kn::random::initialize(42);
+    argparse::ArgumentParser args("cpp-benchmark");
 
-    printf("starting benchmark simulation\n");
+    int case_number = 0;
+    args.add_argument("case_number")
+        .help("Benchmark case to be simulated")
+        .scan<'i', int>()
+        .default_value(1)
+        .choices(1, 2, 3, 4)
+        .store_into(case_number);
 
-    ccp::Simulation sim(ccp::Parameters::case_1());
+    std::string data_path{"../data"};
+    args.add_argument("-d", "--data")
+        .help("Path to folder with cross section data")
+        .default_value(data_path)
+        .store_into(data_path);
+
+    args.parse_args(argc, argv);
+
+    printf("Starting benchmark case %d simulation\n", case_number);
+    printf("Data path set to %s\n", data_path.c_str());
+
+    ccp::Simulation sim(get_case_parameters(case_number), data_path);
     sim.run();
 
     return 0;
 }
-
--- src/simulation.h
+++ src/simulation.h
@@ -1,17 +1,21 @@
 #ifndef SIMULATION_H
 #define SIMULATION_H
 
+#include <string>
+
 #include "parameters.h"
 
 namespace ccp {
 
 class Simulation {
 public:
-    explicit Simulation(const Parameters& parameters) : parameters_(parameters) {};
+    explicit Simulation(const Parameters& parameters, const std::string& data_path)
+        : parameters_(parameters), data_path_(data_path) {};
     void run();
 
 private:
     Parameters parameters_;
+    std::string data_path_;
 };
 
 } // ccp
