39b7492dbf26f32a27dd6f33cf269424728c159b
Lui Habl <txaicalvoso@gmail.com>
2024-11-24 22:41:47 -0300

    Add args to plot script

--- scripts/plot_results.py
+++ scripts/plot_results.py
@@ -1,13 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog='plot_results',
+    description='Plot benchmark and calculation results')
+
+parser.add_argument("out",
+                    help="Path to folder containing the calculation data.")
+
+parser.add_argument("-d", "--data_path", help="Path to folder with benchmark data",
+                    default="../data")
+
+args = parser.parse_args()
 
 L = 6.7e-2
 
-de = np.genfromtxt("../cmake-build-release/density_e.txt")
-di = np.genfromtxt("../cmake-build-release/density_i.txt")
+de = np.genfromtxt(f"{args.out}/density_e.txt")
+di = np.genfromtxt(f"{args.out}/density_i.txt")
 x = np.linspace(0, L, de.size)
 
-data_benchmark_1 = np.genfromtxt("../data/Benchmark_A.csv", delimiter=' ')
+data_benchmark_1 = np.genfromtxt(f"{args.data_path}/Benchmark_A.csv", delimiter=' ')
 xb = data_benchmark_1[:, 0]
 density_e_b = data_benchmark_1[:, 1]
 density_i_b = data_benchmark_1[:, 4]
